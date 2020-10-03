c326a13433d8d2159f734d4f8e1c0ca10e18e307
Bruno Correia <oliveirb@coventry.ac.uk>
2020-10-03 19:09:38 +0000

    Created module & unit tests for 'roles' table.

---
+++ unitTests/test-roles.js
@@ -0,0 +1,15 @@
+import test from 'ava'
+import { Roles } from '../modules/roles.js'
+
+test('ROLES : error if invalid role ID', async test => {
+	test.plan(1)
+	const role = await new Roles('website.db')
+	try {
+		await role.getRole(99)
+		test.fail('error not thrown')
+	} catch (err) {
+		test.is(err.message, 'No matching id', 'incorrect error message')
+	} finally {
+		role.close()
+	}
+})
