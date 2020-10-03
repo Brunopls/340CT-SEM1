f45750a2747f219fd080a9bfd29482e54e54323c
Bruno Correia <oliveirb@coventry.ac.uk>
2020-10-03 19:11:32 +0000

    Added route for 'roles' GET method.

--- routes/secure.js
+++ routes/secure.js
@@ -1,6 +1,9 @@
 
 import Router from 'koa-router'
 
+import { Roles } from '../modules/roles.js'
+const dbName = 'website.db'
+
 const secureRouter = new Router({ prefix: '/secure' })
 
 secureRouter.get('/', async ctx => {
@@ -11,6 +14,52 @@ secureRouter.get('/', async ctx => {
 	} catch(err) {
 		ctx.hbs.error = err.message
 		await ctx.render('error', ctx.hbs)
+	}
+})
+
+/**
+ * The script to get all roles.
+ *
+ * @name Get Roles Script
+ * @route {GET} /roles
+ */
+secureRouter.get('/roles', async ctx => {
+	const roles = await new Roles(dbName)
+	try {
+		const data = await roles.getRoles()
+		ctx.body = data
+
+		//if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in&referrer=/secure')
+		//await ctx.render('secure', ctx.hbs)
+	} catch(err) {
+		ctx.hbs.error = err.message
+		await ctx.render('error', ctx.hbs)
+	} finally {
+		roles.close()
+	}
+})
+
+/**
+ * The script to get all roles.
+ *
+ * @name Get Roles Script
+ * @route {GET} /roles
+ */
+secureRouter.get('/roles', async ctx => {
+	if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in&referrer=/secure')
+
+	const rolesTable = await new Roles(dbName)
+	try {
+
+		const roles = await rolesTable.getRole(1)
+		ctx.hbs.body = roles
+
+
+	} catch(err) {
+		ctx.hbs.error = err.message
+		await ctx.render('error', ctx.hbs)
+	} finally {
+		rolesTable.close()
 	}
 })
 
